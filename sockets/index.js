c4843951d94f32aa526bb56f61f7ce0d1a2440bb
Salman Nissar <salmansecondry@gmail.com>
2026-07-17 21:16:36 +0530

    added socketAuth verification before making socket connection

--- sockets/index.js
+++ sockets/index.js
@@ -1,12 +1,21 @@
 const { WebSocketServer } = require("ws");
+const verifySocketToken = require("./socketAuth");
+
 function initWebSocket(server) {
-  const wss = new WebSocketServer ({server: server,});
-  console.log("connection found")
-  wss.on("connection" , (socket) =>{
-    console.log("client connected")
-  })
+  const wss = new WebSocketServer({ server: server });
+  console.log("connection found");
 
+  wss.on("connection", (socket, req) => {
+    const user = verifySocketToken(req);
+    if (!user) {
+      socket.close();
+      return;
+    }
+    socket.userId = user.Id;
+    console.log(`${user.id}, ${socket.userId}, connected`);
 
+    console.log("client connected");
+  });
 }
 
 module.exports = initWebSocket;
--- sockets/socketAuth.js
+++ sockets/socketAuth.js
@@ -1,3 +1,20 @@
-const {webSocketServer} = require("ws")
-const authenticate = require("../middleware/authMiddleware")
-const JWT_VERIFY = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const { URL } = require("url");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function verifySocketToken(req) {
+  //I refuse to parse a URL unless it looks complete (has a protocol + domain)." all the data comes from req.url and the string just completes it
+  const parsedUrl = new URL(req.url, "http://localhost");//new url a keyword without it we will pass only a string
+  const token = parsedUrl.searchParams.get("token");
+  if (!token) {
+    return null;
+  }
+  try {
+    
+    const decoded = jwt.verify(token, JWT_SECRET);
+    return decoded;
+  } catch (err) {
+    return null;
+  }
+}
+module.exports = verifySocketToken;
