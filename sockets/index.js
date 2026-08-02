1c4a1283a3bb2a22e153a2584794f3b15248feee
Salman Nissar <salmansecondry@gmail.com>
2026-08-02 13:02:23 +0530

    code readabilty in process

--- sockets/index.js
+++ sockets/index.js
@@ -4,9 +4,7 @@ const handleMessage = require("./handlers/messageHandler");
 const onlineUsers = require("../sockets/utils/onlineUsers");
 const deliverPendingMessages = require("../sockets/handlers/deliverPendingMessages");
 const handleMarkAsRead = require("./handlers/readHandler");
-const handleTyping = require("../sockets/handlers/typeHandler")
-
-
+const handleTyping = require("../sockets/handlers/typeHandler");
 
 function initWebSocket(server) {
   const wss = new WebSocketServer({ server: server });
@@ -19,37 +17,34 @@ function initWebSocket(server) {
       return;
     }
     socket.userId = user.id;
-    onlineUsers.set(socket.userId, socket);   
+    onlineUsers.set(socket.userId, socket);
     console.log(`${user.id}, ${socket.userId}, connected`);
     deliverPendingMessages(socket);
 
     console.log("client connected");
-
-
     socket.on("message", (data) => {
       const text = data.toString().trim();
-      
+
       if (!text) {
         return;
-      } 
+      }
       try {
         const msg = JSON.parse(text);
         console.log("Sender's userId:", socket.userId);
-        if(msg.type === "dm"){
-        handleMessage(msg, socket);
-       } else if(msg.type === "markAsRead"){
-        handleMarkAsRead(msg, socket)
-       }
-       else if(msg.type === "typing"){
-        handleTyping(msg, socket)
-       }
+        if (msg.type === "dm") {
+          handleMessage(msg, socket);
+        } else if (msg.type === "markAsRead") {
+          handleMarkAsRead(msg, socket);
+        } else if (msg.type === "typing") {
+          handleTyping(msg, socket);
+        }
       } catch (err) {
         console.error("Invalid JSON received:", err.message);
       }
     });
-    socket.on("close", () =>{
-      onlineUsers.delete(socket.userId)
-    })
+    socket.on("close", () => {
+      onlineUsers.delete(socket.userId);
+    });
   });
 }
 
